695e340a059560fed3e0d08ae941ba2ff83bc532
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2021-10-20 01:08:18 +0400

    Fixing the timing for scheduler

--- src/app/application.cpp
+++ src/app/application.cpp
@@ -1,73 +1,79 @@
 ﻿#include "stdafx.h"
 
-#include <scheduler.h>
+#include <qapplication.h>
 #include <qt_display.h>
+#include <scheduler.h>
+#include <std_timer.h>
 
-lcd::scheduler g_scheduler{};
-size_t g_timing_ratio = 1;
+// smaller value will decrease the speed of the simulation
+constexpr double g_timing_ratio = .000001f;
+lcd::scheduler	 g_scheduler { []() -> std::unique_ptr<lcd::i_timer> {
+	std::unique_ptr<lcd::i_timer>tmr = std::make_unique<lcd::std_timer>();
+	tmr->set_prescaler(g_timing_ratio);
+	return std::move(tmr);
+} };
 std::atomic<bool> g_exit_flag = false;
 
 int main(int argc, char** argv)
 {
-	//QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-//	lcd::qt_display panel(nullptr);
-//	lcd::lcd_controller_ptr controller = std::move(std::make_shared<lcd::lcd_controller>());
-//	panel.set_controller(controller);
-//
-////	panel.set_char_at(0, 0, 'a');
-////	panel.set_char_at(1, 0, 'b');
-////	panel.set_char_at(0, 4, 'c');
-////	panel.set_char_at(3, 7, 'd');
-////	panel.set_char_at(3, 0, 'e');
-////	panel.set_char_at(3, 3, 'f');
-////	panel.set_char_at(2, 7, 'g');
-////	panel.set_char_at(1, 10, 'h');
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::rs)].set_voltage(5.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data0)].set_voltage(5.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data1)].set_voltage(0.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data2)].set_voltage(0.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data3)].set_voltage(0.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data4)].set_voltage(0.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data5)].set_voltage(5.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data6)].set_voltage(5.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::data7)].set_voltage(0.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::en)].set_voltage(5.0f);
-//	controller->m_port.m_pins[static_cast<int>(lcd::lcd_controller::pinout::en)].set_voltage(0.0f);
-//	panel.show();
+	lcd::qt_display			panel(nullptr);
+	lcd::lcd_controller_ptr controller = std::move(std::make_shared<lcd::lcd_controller>());
+	panel.set_controller(controller);
 
+	//	panel.set_char_at(0, 0, 'a');
+	//	panel.set_char_at(1, 0, 'b');
+	//	panel.set_char_at(0, 4, 'c');
+	//	panel.set_char_at(3, 7, 'd');
+	//	panel.set_char_at(3, 0, 'e');
+	//	panel.set_char_at(3, 3, 'f');
+	//	panel.set_char_at(2, 7, 'g');
+	//	panel.set_char_at(1, 10, 'h');
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::rs) ].set_voltage(5.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data0) ].set_voltage(5.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data1) ].set_voltage(0.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data2) ].set_voltage(0.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data3) ].set_voltage(0.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data4) ].set_voltage(0.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data5) ].set_voltage(5.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data6) ].set_voltage(5.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data7) ].set_voltage(0.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::en) ].set_voltage(5.0f);
+	controller->m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::en) ].set_voltage(0.0f);
+	panel.show();
 
 	std::cout << "starting in 3 seconds" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds{ 3 });
+	std::this_thread::sleep_for(std::chrono::seconds { 3 });
 
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
-	g_scheduler.add_task([] {}, std::chrono::microseconds(37));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
+	g_scheduler.add_task([] {}, std::chrono::microseconds(1));
 
 	std::chrono::time_point last_tick = std::chrono::system_clock::now();
-	std::thread scheduler_loop([&last_tick]
-		{
-			while (!g_exit_flag)
+	std::thread				scheduler_loop([ &last_tick ] {
+		g_scheduler.start();
+		while (!g_exit_flag)
 			{
-				std::chrono::duration elapsed = std::chrono::system_clock::now() - last_tick;
-				last_tick += elapsed;
-				elapsed /= g_timing_ratio;
-				g_scheduler.tick(elapsed);
+				// std::chrono::duration elapsed = std::chrono::system_clock::now() - last_tick;
+				// last_tick += elapsed;
+				// std::chrono::duration<double> doubleElapsed = elapsed;
+				// doubleElapsed /= g_timing_ratio;
+				g_scheduler.tick();
 			}
-		}
-	);
-//	app.exec();10
-	while (true);
-	g_exit_flag = true;
+	});
+	int result = app.exec();
+	g_exit_flag			   = true;
 	scheduler_loop.join();
+	return result;
 }
--- src/lcd_logic/scheduler.cpp
+++ src/lcd_logic/scheduler.cpp
@@ -1,33 +1,40 @@
-#pragma once
+#include <stdafx.h>
 
 #include <scheduler.h>
 
 namespace lcd
 {
-	scheduler::scheduler()
-		: m_task_id_counter{ 0 }
-	{}
-
-	void scheduler::add_task(task_t task, std::chrono::nanoseconds after)
+	scheduler::scheduler(std::function<std::unique_ptr<i_timer>()> timer_factory)
+		: m_timer(timer_factory()), m_task_id_counter { 0 }
 	{
-		m_tasks.push_back({ task, after, m_task_id_counter++, std::chrono::system_clock::now()});
 	}
 
-	void scheduler::tick(std::chrono::nanoseconds delta)
+	void scheduler::add_task(task_t task, duration_t delay)
 	{
-		auto it = m_tasks.begin();
+		m_tasks.push_back({ task, delay, m_task_id_counter++, std::chrono::system_clock::now() });
+	}
+
+	void scheduler::start() { m_timer->delta(); }
+
+	void scheduler::tick()
+	{
+		duration_t delta = m_timer->delta();
+		auto	   it	 = m_tasks.begin();
 		while (it != m_tasks.end())
-		{
-			auto tmp = it;
-			++it;
-			std::get<1>(*tmp) -= delta;
-			if (std::get<1>(*tmp) < std::chrono::nanoseconds{ 0 })
 			{
-				std::cout << std::get<2>(*tmp) << " : task finished in " <<
-					std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(std::chrono::system_clock::now() - std::get<3>(*tmp)).count() << " milliseconds" << std::endl;
-				std::get<0>(*tmp)();
-				m_tasks.erase(tmp);
+				auto tmp = it;
+				++it;
+				std::get<1>(*tmp) -= delta;
+				if (std::get<1>(*tmp) < std::chrono::nanoseconds { 0 })
+					{
+						std::cout << std::get<2>(*tmp) << " : task finished in "
+								  << std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(
+										 std::chrono::system_clock::now() - std::get<3>(*tmp))
+										 .count()
+								  << " milliseconds" << std::endl;
+						std::get<0> (*tmp)();
+						m_tasks.erase(tmp);
+					}
 			}
-		}
 	}
-}
+} // namespace lcd
