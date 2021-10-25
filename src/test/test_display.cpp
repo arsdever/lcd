f1719bae684f6397ee77320215f848d7eae3c76e
Arsen <arsen.gharagyozyn.96@gmail.com>
2021-10-25 13:59:35 +0400

    Implements read_busy_flag_and_address (#21)
    * Implements read_busy_flag_and_address
    
    * Test for reading the address counter

--- src/test/test_display.cpp
+++ src/test/test_display.cpp
@@ -44,11 +44,25 @@ namespace lcd
 				}
 		}
 
-		inline void command(bool rw, uint8_t byte)
+		inline uint8_t read_bus() const
+		{
+			uint8_t value = 0;
+			for (int i = static_cast<int>(lcd_controller::pinout::data7);
+				 i >= static_cast<int>(lcd_controller::pinout::data0);
+				 --i)
+				{
+					value <<= 1;
+					value |= static_cast<uint8_t>(digital_operation::read(m_controller.m_port.m_pins[ i ]));
+				}
+			return value;
+		}
+
+		inline void command(bool rs, bool rw, uint8_t byte)
 		{
 			m_controller.m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::rw) ].set_voltage(rw ? 5.0f
 																										   : 0.0f);
-			m_controller.m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::rs) ].set_voltage(5.0f);
+			m_controller.m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::rs) ].set_voltage(rs ? 5.0f
+																										   : 0.0f);
 			digital_operation::write(m_controller.m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data0) ],
 									 byte & 1);
 			digital_operation::write(m_controller.m_port.m_pins[ static_cast<int>(lcd::lcd_controller::pinout::data1) ],
@@ -84,10 +98,13 @@ namespace lcd
 	BOOST_AUTO_TEST_CASE(print_symbol_test)
 	{
 		test_framework framework;
+		// 1s real = 1000us simulated
+		framework.set_prescaler(.001f);
 		framework.start();
 
-		framework.command(false, 'a');
+		framework.command(true, false, 'a');
 
+		std::this_thread::sleep_for(std::chrono::milliseconds(40));
 		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 'a');
 		framework.stop();
 	}
@@ -99,23 +116,46 @@ namespace lcd
 		framework.set_prescaler(.001f);
 		framework.start();
 
-		framework.command(false, 'a');
-		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 'a');
+		framework.command(true, false, 'a');
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 0);
 		BOOST_CHECK_EQUAL(framework.controller().is_busy(), true);
 
-		framework.command(false, 'b');
-		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 'a');
+		framework.command(true, false, 'b');
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(1), 0);
 		BOOST_CHECK_EQUAL(framework.controller().is_busy(), true);
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		BOOST_CHECK_EQUAL(framework.controller().is_busy(), true);
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 0);
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(1), 0);
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		BOOST_CHECK_EQUAL(framework.controller().is_busy(), false);
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 'a');
 
-		framework.command(false, 'b');
-		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(0), 'b');
+		framework.command(true, false, 'b');
 		BOOST_CHECK_EQUAL(framework.controller().is_busy(), true);
+		std::this_thread::sleep_for(std::chrono::milliseconds(40));
+		BOOST_CHECK_EQUAL(framework.controller().symbol_at_ddram(1), 'b');
+		BOOST_CHECK_EQUAL(framework.controller().is_busy(), false);
+		framework.stop();
+	}
+
+	BOOST_AUTO_TEST_CASE(busy_flag)
+	{
+		test_framework framework;
+		// 1s real = 1000us simulated
+		framework.set_prescaler(.001f);
+		framework.start();
+
+		framework.command(true, false, 'a');
+		BOOST_CHECK_EQUAL(framework.controller().is_busy(), true);
+		framework.command(false, true, 0);
+		BOOST_CHECK_EQUAL(framework.read_bus(), 0x80);
+		std::this_thread::sleep_for(std::chrono::milliseconds(40));
+		BOOST_CHECK_EQUAL(framework.controller().is_busy(), false);
+		framework.command(false, true, 0);
+		BOOST_CHECK_EQUAL(framework.read_bus(), 0x01);
 		framework.stop();
 	}
 
