1611c5aa942dcee6a5f11fa782111cc5db87e011
Arsen <arsen.gharagyozyn.96@gmail.com>
2021-10-28 18:23:54 +0400

    Implements display_on_off_control (#39)

--- src/lcd_logic/display.cpp
+++ src/lcd_logic/display.cpp
@@ -20,27 +20,44 @@ namespace lcd
 
 	const char& display::symbol_at(size_t row, size_t column) const
 	{
+		if (m_controller->lines() == lcd_controller::display_lines_mode_enum::single_line && row % 2)
+			{
+				return ' ';
+			}
+
+		uint8_t addr = address_of_symbol(row, column);
+		if (addr == 0xff)
+			{
+				lcd_assert(false,
+						   std::string("Invalid address returned for display symbol at: ") + std::to_string(column) +
+							   " " + std::to_string(row));
+
+				return ' ';
+			}
+
+		return m_controller->symbol_at_ddram(addr);
+	}
+
+	uint8_t display::address_of_symbol(size_t row, size_t column) const
+	{
 		if (row < 0 || row > m_height || column < 0 || column > m_width)
 			{
 				lcd_assert(false,
 						   std::string("The requested symbol is out of the lcd space: ") + std::to_string(column) +
 							   " " + std::to_string(row));
 
-				return m_controller->symbol_at_ddram(0x00);
+				return uint8_t(0xff);
 			}
 
 		if (m_controller->lines() == lcd_controller::display_lines_mode_enum::single_line)
 			{
-				if (row % 2)
-					return ' ';
-
 				// TODO: the display scroll is not encounted
-				return m_controller->symbol_at_ddram(row / 2 * m_width + column);
+				return static_cast<uint8_t>(row / 2 * m_width + column);
 			}
 		else
 			{
-				return m_controller->symbol_at_ddram(
-					row % 2 * 0x40 + (row / 2 * m_width + column + m_controller->scroll_size()) % (2 * m_width));
+				return static_cast<uint8_t>(row % 2 * 0x40 +
+											(row / 2 * m_width + column + m_controller->scroll_size()) % (2 * m_width));
 			}
 	}
 
@@ -62,15 +79,29 @@ namespace lcd
 
 	std::array<char, 8> display::get_symbol_at(size_t row, size_t column) const
 	{
-		char				symb = symbol_at(row, column);
-		void*				source;
-		std::array<char, 8> result;
+		if (m_controller->cursor_state() == lcd_controller::cursor_mode_enum::box &&
+			address_of_symbol(row, column) == m_controller->cursor_position())
+			{
+				return {
+					char(0xff), char(0xff), char(0xff), char(0xff), char(0xff), char(0xff), char(0xff), char(0xff)
+				};
+			}
+		else
+			{
+				char				symb = symbol_at(row, column);
+				void*				source;
+				std::array<char, 8> result;
 
-		symb < 8 ? source = m_controller->m_cgram.data() + 8 * symb
-				 : source = m_controller->m_cgrom.data() + 8 * (symb - 8);
+				symb < 8 ? source = m_controller->m_cgram.data() + 8 * symb
+						 : source = m_controller->m_cgrom.data() + 8 * (symb - 8);
 
-		std::memcpy(result.data(), source, 8);
-		return result;
+				std::memcpy(result.data(), source, 8);
+				if (m_controller->cursor_state() == lcd_controller::cursor_mode_enum::line &&
+					address_of_symbol(row, column) == m_controller->cursor_position())
+					result.data()[ 7 ] = char(0xff);
+
+				return result;
+			}
 	}
 
 #pragma endregion i_character_data
