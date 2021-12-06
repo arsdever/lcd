0e6fbf3dfe92269f9d21456ef680b8f9923ec420
Arsen <arsen.gharagyozyn.96@gmail.com>
2021-12-06 23:16:34 +0400

    Non spdlog compilation fix (#76)

---
+++ src/logging/std_log_history_sink.cpp
@@ -0,0 +1,6 @@
+#include "factory/log_history_sink_factory.h"
+
+namespace lcd
+{
+	log_history_sink_ptr log_history_sink_factory::create(log_history_wptr history) { return nullptr; }
+} // namespace lcd
